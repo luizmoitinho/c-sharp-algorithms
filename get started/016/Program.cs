083b731ec0822b80e21dbf44efdc55bf2ba38131
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-29 12:58:51 -0300

    feat - use out for return methods

---
+++ get started/016/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10, num2 = 2;
+            int quociente, resto;
+            quociente = divide(num1,num2, out resto);
+            Console.WriteLine("Quociente: {0} | Resto: {1}",quociente,resto);
+        }
+
+        static int divide(int dividendo, int divisor, out int resto){
+            int quociente;
+            quociente = dividendo/divisor;
+            resto = dividendo%divisor;
+            return quociente;
+        }
+    }
+}
