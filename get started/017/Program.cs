906ef8ff1ebcba7261c32dae6c77272f4def9350
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-29 13:08:40 -0300

    feat - methods with 0 or than params

---
+++ get started/017/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           soma(1,2,3);
+        }
+
+        static void soma(params int[] n)
+        {
+            int res = 0;
+            foreach(int elem in n)
+                res += elem;
+            
+           Console.WriteLine("Valor total da soma: {0}",res);
+            
+        }
+    }
+}
