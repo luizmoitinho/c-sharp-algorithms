7af4c48a5618566f6b017cc535618629c4f70353
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-29 00:16:54 -0300

    feat - two-dimensional arrays

--- 011/aula11.cs
+++ 011/aula11.cs
@@ -3,16 +3,21 @@ using System;
 class Aula11{
     static void Main(){
         
-        int[] arrayNumeros = new int[5];
+        int[,] arrayNumeros = new int[3,3];
 
-        arrayNumeros[0] = 20;
-        arrayNumeros[1] = 40;
-        arrayNumeros[2] = 50;
-        arrayNumeros[3] = 60;
-        arrayNumeros[4] = 70;
+        arrayNumeros[0,0] = 20;
+        arrayNumeros[0,1] = 40;
+        arrayNumeros[0,2] = 50;
 
-        for(int i =0; i<5 ; i++ )
-            Console.Write("{0} ", arrayNumeros[i]);
+        arrayNumeros[1,0] = 60;
+        arrayNumeros[1,1] = 70;
+        arrayNumeros[1,2] = 60;
+
+        for(int i = 0; i<3 ; i++ ){
+          for(int j = 0; j<3; j++)
+              Console.Write("{0} ",arrayNumeros[i,j]);
+          Console.WriteLine("");
+        }
     }
 
 }
