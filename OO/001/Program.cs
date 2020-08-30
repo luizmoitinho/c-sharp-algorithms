a037c524e87478bddcfb8a43cc280de5d4c871f0
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-30 17:26:56 -0300

    feat - create first class in C#

---
+++ OO/001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador j1 = new Jogador();
+            j1.nome = "fox";
+
+            Console.WriteLine("Dados do jogador");
+            j1.energia=90;
+            Console.WriteLine(j1.toString());
+
+        }
+    }
+
+    public class Jogador
+    {
+
+        public string nome;
+        public int energia = 100;
+        public bool vida = true;
+
+        public string toString()
+        {
+            return (
+                "Nome   : " + this.nome + "\n" +
+                "Energia: " + this.energia + "% \n" +
+                "Vivo   ? " + this.vida + "\n"
+            );
+        }
+
+    }
+}
