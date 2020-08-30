df8899ceafc68c69284e24e318d63bcdbc772c5c
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-30 17:52:24 -0300

    refactor - create a class with construct and destruct

--- OO/001/Program.cs
+++ OO/001/Program.cs
@@ -6,12 +6,23 @@ namespace _001
     {
         static void Main(string[] args)
         {
-            Jogador j1 = new Jogador();
-            j1.nome = "fox";
+       
+            String player1 = "", player2 = "";
 
-            Console.WriteLine("Dados do jogador");
-            j1.energia=90;
+            Console.WriteLine("****** INCIALIZANDO O JOGO ***********");
+            System.Console.Write("Nome do player 1: ");
+            player1 =System.Console.ReadLine();
+            System.Console.Write("Nome do player 2: ");
+            player2 =System.Console.ReadLine();
+
+
+            Jogador j1 = new Jogador(player1);
+            Jogador j2 = new Jogador(player2);
+            
+            Console.WriteLine("Player 1");
             Console.WriteLine(j1.toString());
+            Console.WriteLine("Player 2");
+            Console.WriteLine(j2.toString());
 
         }
     }
@@ -20,8 +31,20 @@ namespace _001
     {
 
         public string nome;
-        public int energia = 100;
-        public bool vida = true;
+        public int energia;
+        public bool vida;
+        
+        //Cronstrutor da classe
+        public Jogador(string nome){
+            this.nome = nome;
+            this.energia = 100;
+            this.vida = true;
+        }
+        
+        //Cronstrutor da classe
+        ~Jogador(){
+            System.Console.WriteLine("Jogador {0} foi destuido!",this.nome);
+        }
 
         public string toString()
         {
