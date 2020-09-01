1cd067d3622486e22cadc8c7413acb372d19eacb
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-31 23:04:55 -0300

    feat - abstract class and methods

---
+++ OO/007/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro1 = new Carro();
+            carro1.aceleracao(10);
+            System.Console.WriteLine(carro1.toString());
+        }
+
+        abstract class Veiculo{
+            protected int velocidadeMax;
+            protected int velocidadeAtual;
+            protected bool ligado;
+
+            public Veiculo(){
+                this.ligado = false;
+                this.velocidadeAtual = 0;
+            }
+
+            public void setLigado(bool ligado){
+                this.ligado = ligado;
+            }
+            abstract public void aceleracao(int acelaracao);
+            
+        }
+        class Carro:Veiculo{
+
+            public Carro():base(){
+                this.velocidadeMax = 120;
+            }
+
+            override public void aceleracao(int acelaracao){
+                this.velocidadeAtual += acelaracao;
+            }
+
+            public string toString(){
+                return(
+                    "Vel. maxima....: "+ this.velocidadeMax +"\n"+
+                    "Vel. atual.....: "+ this.velocidadeAtual +"\n"+
+                    "Ligado.........: "+ (this.ligado ? "Sim" : "Não" ) +"\n"
+
+                );
+            }
+        }
+    }
+}
