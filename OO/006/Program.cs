669143662fc1d38a67839d910fde93a113079c6e
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-31 21:48:16 -0300

    feat - inheritance order between classes

---
+++ OO/006/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derivada2 d2 = new Derivada2();
+
+        }
+    }
+
+    class Base{
+        public Base(){
+            System.Console.WriteLine("Construtor da classe base.");
+        }
+    }
+
+    class Derivada1:Base{
+        public Derivada1(){
+             System.Console.WriteLine("Construtor da classe derivada 1.");
+        }
+
+    }
+
+    class Derivada2:Derivada1{
+        public Derivada2(){
+             System.Console.WriteLine("Construtor da classe derivada 2.");
+        }
+
+    }
+
+}
