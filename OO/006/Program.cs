7ed3d4479863765ae17304d512fcf90a71221045
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-08-31 22:12:29 -0300

    feat - virtual methods and Ref with Class

--- OO/006/Program.cs
+++ OO/006/Program.cs
@@ -6,7 +6,17 @@ namespace _006
     {
         static void Main(string[] args)
         {
+            Base Ref; 
+            Derivada1 d1 = new Derivada1();
             Derivada2 d2 = new Derivada2();
+            System.Console.WriteLine("\n\nInstancia de Derivada 1");
+            Ref = d1;
+            Ref.info();
+            System.Console.WriteLine("\n\nInstancia de Derivada 2");
+            Ref = d2;
+            Ref.info();
+           
+
 
         }
     }
@@ -15,6 +25,10 @@ namespace _006
         public Base(){
             System.Console.WriteLine("Construtor da classe base.");
         }
+
+        virtual public void info(){
+            System.Console.WriteLine("Base");
+        }
     }
 
     class Derivada1:Base{
@@ -22,11 +36,19 @@ namespace _006
              System.Console.WriteLine("Construtor da classe derivada 1.");
         }
 
+        override public void info(){
+            System.Console.WriteLine("Derivada1");
+        }
+
     }
-    
+
     class Derivada2:Derivada1{
         public Derivada2(){
              System.Console.WriteLine("Construtor da classe derivada 2.");
+        }
+
+        override public void info(){
+            System.Console.WriteLine("Derivada2");
         }
 
     }
