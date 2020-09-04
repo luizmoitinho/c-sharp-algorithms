25229ec7616baed7a8756b5f087823bdf5668000
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-09-03 23:01:59 -0300

    feat - create a interface

---
+++ OO/009/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+        }
+    }
+
+    public interface Veiculo{
+
+        void ligar();
+        
+        void desligar();
+        void acelerar();
+
+
+    }
+    
+    public interface Combate{
+
+        void disparar();
+
+
+    }
+
+    class Carro: Veiculo, Combate{
+
+        private bool ligado;
+        private int velAtual;
+        private int municao;
+
+        public Carro(){
+            this.ligado = false;
+            this.velAtual = 0;
+            this.municao = 100;
+        }
+
+        public void acelerar(){
+            this.velAtual += 2;
+        }
+
+        public void ligar(){
+            this.ligado = true;
+        }
+
+        public void desligar(){
+            this.ligado = false;
+        }
+
+        public bool getLigado(){
+            return this.ligado;
+        }
+
+        public void disparar(){
+            System.Console.WriteLine("---->>>");
+            this.municao--;
+        }
+
+
+    }
+
+
+}
