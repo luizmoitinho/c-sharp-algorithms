caba07b5ec579bc600cf19c5cfe151ebd29ad01c
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-09-10 00:07:44 -0300

    refactor - create array of structs

--- OO/010/Program.cs
+++ OO/010/Program.cs
@@ -34,8 +34,21 @@ namespace _010
         static void Main(string[] args)
         {
             Console.WriteLine("Structs\n");
-            Carro c1 = new Carro("Fiat","grand sien","branco",2017);
-             System.Console.WriteLine(c1.info());
+            Carro[] arrayCarros = new Carro[3];
+            
+            arrayCarros[0].modelo="HRV";
+            arrayCarros[0].cor="Prata";
+
+            arrayCarros[1].modelo="Golf";
+            arrayCarros[1].cor="Azul";
+
+            arrayCarros[2].modelo="Fiat argo";
+            arrayCarros[2].cor="Preto";
+            
+
+            foreach(Carro c in arrayCarros)
+                System.Console.WriteLine(c.info());
+
 
         }
     }
